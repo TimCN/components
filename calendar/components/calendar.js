b42054d98ff9b918bb2262a62b67dc685f0dcd94
timcn <tim.shanghai@outlook.com>
2016-01-17 20:56:02 +0800

    粗糙版，有时间再来优化

--- calendar/components/calendar.js
+++ calendar/components/calendar.js
@@ -2,6 +2,53 @@
 
 
 
+  //常量
+
+  //月份字典（用于月份头显示）    如 一月为 0 其对应的 MONTH[0]: "1月"
+  var MONTH = new Array(
+    "1月",
+    "2月",
+    "3月",
+    "4月",
+    "5月",
+    "6月",
+    "7月",
+    "8月",
+    "9月",
+    "10月",
+    "11月",
+    "12月"
+  );
+
+  var WEEKHEADERS = new Array(
+    "一",
+    "二",
+    "三",
+    "四",
+    "五",
+    "六",
+    "日"
+  );
+  //星期枚举 (用于计算)  如星期日   为 WEEK[0]:7
+  var WEEK = new Array(
+    7,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+  );
+
+
+  //记录需要特殊处理的日子，这里用于记录赛事日子
+  var SPECIAL_DATES={};
+  var monthPrefix  = "monthCell" + Math.ceil(Math.random()*10000);
+  var width,height;
+  var monthTHClass="calendarMonthCell",
+      monthTHActiveClass="calendarMonthTHActive";
+  var dateContainerID = "calendarDateContainer";
+
 
 
   //载入样式
@@ -16,35 +63,210 @@
     header.appendChild(style);
   })();
 
-  var monthTHID = "monthTH" + Math.ceil(Math.random()*10000);
 
-  var monthPrefix  = "monthcell_";
-  var monthTHClass = "calendarMonthCell"
-  var monthTHActiveClass = "calendarMonthTHActive";
 
-  var currentDate  = new Date().getDate();
-  var  startDate,
-         endDate;
 
-  var MONTH = new Array(
-    "1月",
-    "2月",
-    "3月",
-    "4月",
-    "5月",
-    "6月",
-    "7月",
-    "8月",
-    "9月",
-    "10月",
-    "11月",
-    "12月"
-  )
+
+
+  /* 动态创建月份头部，区间是[fromMonth,toMonth]
+   * @param {Date} fromMonth 开始的月份
+   * @param {Date} toMonth   结束的月份
+   * @param {Date} activeMonth 处于被选中的月份
+   * @param {string} monthClass 月份元素的样式类
+   * @param {string} monthActiveClass 被选中的月份元素的样式类
+   * @param {string} monthIdPrefix 月份元素ID的前缀
+   * @param {int} monthContainerWidth 月份父元素宽度(单位：px)
+   * @param {string} monthContainerClass 月份父元素样式类
+   * @return {string} 返回一串html
+   */
+  function setupMonth (fromMonth, toMonth, activeMonth, monthClass, monthActiveClass, monthIdPrefix, monthContainerWidth,monthContainerHeight) {
+
+    //初始化
+    var monthCount = (toMonth.getFullYear()-fromMonth.getFullYear()) * 12 + toMonth.getMonth()-fromMonth.getMonth() + 1;
+    var margins = (monthCount-1)*3;
+    var monthCellWidth = ( monthContainerWidth - ( ( monthCount -1 ) * 3 ) ) / monthCount;
+
+    if(!activeMonth){
+      activeMonth = fromMonth;
+    }
+    var monthHTML = '  <div id="calendarMonth" style="width:'+monthContainerWidth+'px;height:' + monthContainerHeight + 'px;">';
+
+    var tempClass, monthId, monthStyle;
+    for (var i = 0; i < monthCount; i++) {
+      tempClass = monthClass;
+      monthStyle = 'width:' + monthCellWidth + 'px;height:'+monthContainerHeight+'px;line-height:'+monthContainerHeight+'px;';
+
+      //当前被选中的月份添加样式
+      if(activeMonth.getFullYear() == fromMonth.getFullYear() && activeMonth.getMonth() == fromMonth.getMonth()){
+        tempClass +=" " + monthActiveClass;
+      }
+
+      //最后一个没有margin-right
+      if(i==monthCount-1){
+        monthStyle+="margin-right:0px;"
+      }
+
+      //生成月份元素的id
+      monthId = monthIdPrefix + "_" + fromMonth.getFullYear()+ "_" + fromMonth.getMonth();
+
+      monthHTML += '<div id="' + monthId + '" style=' + monthStyle + ' class="' + tempClass + '" onclick="window.calendar.onMonthClick(\'' + monthId + '\')">'
+                + MONTH[fromMonth.getMonth()]
+                + '</div>';
+
+      fromMonth= new Date(fromMonth.getFullYear(),fromMonth.getMonth()+1,1);
+    }
+    monthHTML += '</div>';
+    return monthHTML;
+  }
+
+  /*
+   * @return {string} 返回周(头部)的html字符串
+   */
+  function setupDay (containerWidth,containerHeight,rowClass,rowMarginClass,cellClass) {
+
+    containerWidth = containerWidth - 20;
+    var cellWidth  = containerWidth / 7;
+    var innerHTML ='<div class="' + rowClass + '" style="width:'+containerWidth+'px;height:'+(containerHeight-1)+'px;"><div style="height:' + (containerHeight-1) + 'px;width:' + containerWidth + 'px;">';
+    var styles = "width:"+cellWidth+"px;height:" + (containerHeight-1) +"px;line-height:"+ (containerHeight-1) +"px;";
+    for(var i = 0; i < 7; i++){
+      innerHTML +='<div style="' + styles + '" class="' + cellClass + '">'
+                 + WEEKHEADERS[i]
+                 +' </div>';
+    }
+    innerHTML +='</div></div>'
+    return innerHTML;
+  }
+
+  /*
+   *@param {Date} date 需要计算的月份日期
+   *@return {string} 返回本月所以日期的html字符串
+   */
+  function setupDate (date,dateContainerWidth,dateContainerHeight,dateClass,rowClass) {
+    var innerHTML = '<div id ="'+dateContainerID+'" style="width:'+dateContainerWidth+'px;height:'+dateContainerHeight+'px">';
+        innerHTML += getDatesHTML(date,dateContainerWidth,dateContainerHeight,dateClass,rowClass);
+        innerHTML += '</div>';
+
+    return innerHTML;
+  }
+  function getDatesHTML (date,dateContainerWidth,dateContainerHeight,dateClass,rowClass) {
+    //计算出单个的高度
+    var firstDate = new Date(date.getFullYear(),date.getMonth(),1,2);
+    var lastDate  = new Date(date.getFullYear(),date.getMonth()+1,1,2);
+        lastDate  = new Date(lastDate.setDate(lastDate.getDate()-1));
+    var offsetDates = WEEK[firstDate.getDay()] - 1;
+    var lines = Math.ceil((lastDate.getDate() +offsetDates) / 7);
+
+    //日期单位高度
+    var dateHeight = (dateContainerHeight-lines + 1) / lines;
+    //日期单位宽度
+    var dateWidth  = (dateContainerWidth-20-14) / 7;
+
+    var dateHTML ='',
+        rowStyles;
+    var currentDate = new Date(date.getFullYear(),date.getMonth(),1);
+
+    //将起始日确定到星期一
+    currentDate= new Date(currentDate.setDate(currentDate.getDate()-offsetDates));
+
+
+    for(var i = 0; i < lines; i++) {
+
+      rowStyles = 'height:' + dateHeight + 'px;width:'+(dateContainerWidth-20)+'px;';
+
+      if(i == lines - 1){
+
+        rowStyles +="border-bottom:none;";
+
+      }
+
+      dateHTML += '<div class="' + rowClass + '" style="' + rowStyles + '">';
+
+      var styles;
+
+
+      var currentDay = WEEK[currentDate.getDay()];
+
+      for(var j = currentDay; j < 8; j++) {
+
+          styles = "position:relative;width:"+dateWidth+"px;height:" + (dateHeight-4) +"px;line-height:"+ dateHeight +"px;";
+
+          var hoverDivHTML ="";
+          var classes = dateClass;
+          if(currentDate.getMonth()!=date.getMonth()){
+
+            //不是当前月份的日期
+            styles += "background:none;color:#5c5c5c;"
+
+          } else {
+
+            hoverDivHTML=getHoverDivHTML(currentDate,dateWidth, dateHeight);
+            if(hoverDivHTML){
+              classes = "calendarSpecialDate " + classes;
+            }
+          }
+
+
+
+          dateHTML  +='<div style="' + styles + '" class="' + classes + '">'
+                    +'<div>'
+                    +     currentDate.getDate()
+                    +'</div>'
+                    + hoverDivHTML
+                    +' </div>';
+
+          currentDate.setDate(currentDate.getDate()+1);
+      }
+
+      dateHTML +='</div>';
+
+    }
+
+    return dateHTML;
+
+  }
+  function getHoverDivHTML(currentDate,dateWidth, dateHeight){
+    // debugger;
+    var arr = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate();
+    var eventsHTML="";
+    if(SPECIAL_DATES[arr]){
+      var events = SPECIAL_DATES[arr];
+      var width= dateWidth*4,
+          height= dateHeight*1.5;
+      eventsHTML = '<div style="width:' + width + 'px;height:' + height + 'px;top:-' + ( dateHeight * 1.7 ) + 'px;left:-' + ( dateWidth * 3 ) + 'px;" class="dateClassHover">'
+      var title ='<div style="margin-bottom:3px;padding-left:10px;border-bottom:red solid 2px;width:'+(width-10)+'px;height:'+(height*0.33)+'px;text-align:left;line-height:'+(height*0.33)+'px;">'
+                + (currentDate.getMonth()+1) + '月' + currentDate.getDate() + '号'
+                +'</div>';
+
+      var contents = "";
+      for(var i = 0; i < events.length; i++){
+        contents+='<div style="padding-left:10px;solid 2px;width:'+(width-10)+'px;height:'+(height*0.2)+'px;text-align:left;line-height:'+(height*0.2)+'px;">'
+                 + events[i]
+                 + '</div>';
+      }
+      eventsHTML += title + contents + '</div>';
+    }
+    // var width= dateWidth*4,
+    //     height= dateHeight*1.5;
+    // var html = '<div style="width:'+(dateWidth*4)+'px;height:'+(dateHeight*1.5)+'px;top:-'+(dateHeight*1.6)+'px;left:-'+(dateWidth*3)+'px;" class="dateClassHover">';
+    // var title ='<div style="padding-left:10px;border-bottom:red solid 2px;width:'+(width-10)+'px;height:'+(height*0.33)+'px;text-align:left;line-height:'+(height*0.33)+'px;">1月14号</div>';
+    // var content1 ='<div style="padding-left:10px;solid 2px;width:'+(width-10)+'px;height:'+(height*0.2)+'px;text-align:left;line-height:'+(height*0.2)+'px;">17:00 EDG vs RNG</div>';
+    // var content2 ='<div style="padding-left:10px;solid 2px;width:'+(width-10)+'px;height:'+(height*0.2)+'px;text-align:left;line-height:'+(height*0.2)+'px;">19:00 VG vs OMG</div>';
+    // // var content3 ='<div style="padding-left:10px;solid 2px;width:'+(width-10)+'px;height:'+(height*0.2)+'px;text-align:left;line-height:'+(height*0.2)+'px;">19:00 VG vs OMG</div>';
+    // html  += title+content1+'</div>';
+    // html  += title+content1+content2+'</div>';
+    // html  += title+content1+content2+content3+'</div>';
+
+    return eventsHTML;
+  }
+
+
+
+
 
 
   function monthClick(monthId){
 
-    var nodes = document.getElementById(monthTHID).childNodes;
+    var nodes = document.getElementById(monthId).parentNode.childNodes;
     for(var i=0;i<nodes.length;i++){
       if(nodes[i].nodeType !=1){
         continue;
@@ -56,67 +278,14 @@
         nodes[i].setAttribute("class", monthTHClass)
       }
     }
-    setDates(monthId.split("_")[1],monthId.split("_")[2])
+
+    var currentMonth = new Date(monthId.split("_")[1],monthId.split("_")[2],1);
+    var innerhtml = getDatesHTML(currentMonth,width,(height*0.75),"calendarDateCell","calendarRow");//75%
+    document.getElementById(dateContainerID).innerHTML = innerhtml;
   }
 
-  function setDates(year,month){
-    currentDate = new Date(year, month, 1);
-    var html = calcDate();
-    document.getElementById("calendarDateRow").innerHTML = html;
-  }
-
-  function calcDate(){
-    //currentDate
-    var beginDate = 1;//currentDate.getDate();
-    var endDate   = getMaxDateInMonth(currentDate);
-
-    //用于记录第一天和最后天的padding样式
-    var offsetDays = {};
-
-    //第一天
-    var offsetDay = new Date(currentDate.getFullYear(),currentDate.getMonth(),1).getDay()-1;
-    if(offsetDay < 0){
-      offsetDay = 7 + offsetDay;
-    }
-    offsetDays[beginDate] = ' style="padding-left:'+(offsetDay*14.285)+'%;"';
-
-    //最后一天
-    offsetDay = endDate.getDay() > 0 ? endDate.getDay() : 7;
-    offsetDays[endDate.getDate()]=' style="padding-right:'+((7-offsetDay)*14.285)+'%;"';
-    var html ="";
-
-    //最后一个星期的第一天
-    var lastWeekFirstDate = endDate.getDate() - offsetDay;
-
-    while (beginDate <= endDate.getDate()) {
-
-      var attrStr = 'class="calendarDateCell"';
-
-      //最后一周   请勿调整其和最后一天的顺序
-      if(beginDate >lastWeekFirstDate){
-        attrStr = 'class="LastCalendarDateCell calendarDateCell"';
-      }
-
-      //第一天/最后一天
-      if(beginDate == 1||beginDate == endDate.getDate()){
-        attrStr += offsetDays[beginDate];
-      }
 
 
-      html  +='<div ' + attrStr + '>'
-            +'   <p>'
-            +     beginDate
-            +'  </p>'
-            +' </div>';
-
-      //最后一周把分割线去掉
-
-
-      beginDate++;
-    }
-
-    return html;
-  }
 
   function AppendMonthActiveClass (domID) {
     var node = document.getElementById(domID);
@@ -126,114 +295,40 @@
 
 
 
-  function installer(id,beginDate,finishDate){
+  function installer(id,beginDate,finishDate,initWidth,initHeight,specialDates){
 
-     //debugger;
-    startDate = new Date(beginDate);
-    endDate = new Date(finishDate);
+    startDate = new Date(parseInt(beginDate.split("-")[0]),(parseInt(beginDate.split("-")[1])-1),parseInt(beginDate.split("-")[2]));
+    endDate = new Date(parseInt(finishDate.split("-")[0]),(parseInt(finishDate.split("-")[1])-1),parseInt(finishDate.split("-")[2]));
     if(startDate == "Invalid Date"|| endDate =="Invalid Date"){
       return;
     }
 
+    SPECIAL_DATES=specialDates
+
     //init currentMonth
-    if(currentDate>endDate || currentDate<startDate){
+    var currentDate = new Date();
+    if(currentDate > endDate || currentDate < startDate){
       currentDate = startDate;
     }
+    width = initWidth-4;
+    height = initHeight-4;
 
 
+    var style = 'style="width:' + width + 'px;height:' + height + 'px;"';
 
-
-    var tableBegin            = '<div class="calendarTable">';
-    var monthTHBegin          = '  <div id="'+monthTHID+'" class="calendarMonthTH">';
-    var monthCellsHTML        = '';
-    var monthTHEnd            = '  </div>';
-    var dayTHHTML             = `  <div class="calendarDayTH">
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             一
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             二
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             三
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             四
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             五
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             六
-                                           </p>
-                                       </div>
-                                       <div class="calendarDayCell">
-                                           <p>
-                                             日
-                                           </p>
-                                       </div>
-                                   </div>`;
-    var dateRowHTML           = '';
+    var tableBegin            = '<div ' + style + ' class="calendarTable">';
+    var monthHTML             = setupMonth(startDate, endDate, currentDate, monthTHClass, monthTHActiveClass, monthPrefix, width, (height*0.14));//h:14%
+    var dayTHHTML             = setupDay(width, (height*0.11), "calendarRow", "calendarRowMargin", "calendarCell");//H:11%
+    var dateRowHTML           = setupDate(currentDate,width,(height*0.75),"calendarDateCell","calendarRow");//75%
     var tableEnd              = '</div>';
 
 
-
-    //月份计算,可能跨年
-    var showMonth  = new Date(startDate.getFullYear(),startDate.getMonth(),1);
-    while (showMonth <= endDate) {
-
-      var tempClass = monthTHClass;
-      if(currentDate.getFullYear() == showMonth.getFullYear() && currentDate.getMonth() == showMonth.getMonth()){
-        tempClass +=" " + monthTHActiveClass;
-      }
-      var mid = monthPrefix + showMonth.getFullYear()+ "_" + showMonth.getMonth();
-
-      //最后一个月无margin-right
-      if(showMonth.getFullYear() == endDate.getFullYear() && showMonth.getMonth() == endDate.getMonth()){
-        tempClass= "lastCalendarDayCell " + tempClass;
-      }
-      monthCellsHTML += '<div id="' + mid + '" class="' + tempClass + '" onclick="window.calendar.onMonthClick(\'' + mid + '\')">'
-                      + '   <p>' + MONTH[showMonth.getMonth()] + '</p>'
-                      + '</div>';
-
-      showMonth= new Date(showMonth.getFullYear(),showMonth.getMonth()+1,1);
-    }
-
-    showMonth = null;
-
-    //计算日期
-    dateRowHTML = calcDate();
-    dateRowHTML ='<div id="calendarDateRow">'
-                +  dateRowHTML
-                +'</div>';
     document.getElementById(id).innerHTML  = tableBegin
-                                           + monthTHBegin
-                                           + monthCellsHTML
-                                           + monthTHEnd
+                                           + monthHTML
                                            + dayTHHTML
                                            + dateRowHTML
                                            + tableEnd ;
   }
-
-  function getMaxDateInMonth(date){
-    var nextMonth = new Date(date.getFullYear(),date.getMonth()+1,1);
-    return new Date(nextMonth - 24*60*60*1000);
-  }
-
-
-
-
 
 
   g.calendar={
